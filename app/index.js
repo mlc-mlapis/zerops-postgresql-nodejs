368c339429a0918208bbd1692b56c04cafcb5769
Miloš Lapiš <mlapis@mlc.cz>
2021-11-29 19:16:08 +0100

    wip(pg): connect

--- app/index.js
+++ app/index.js
@@ -1,17 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const pg = require('pg');
+const {Client} = require('pg');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	/*
-	(async () => {
-		
+const hostname = 'postgresql';
 
+// Function returning an connectionString environment variable of the <hostname> service.
+const getConnectionString = (hostname) => {
+	const connectionString = 'connectionString';
+	const value = env[`${hostname}_${connectionString}`];
+	return value ? value : null;
+}
+
+const pgClient = new Client({connectionString: connectionString});
+
+const connect = async (pgClient) => {
+	const connectionString = getConnectionString(hostname);
+	if (host) {
+		return await pgClient.connect(connectionString);
+	}
+	console.log('... connect failed.');
+	return null;
+}
+
+
+app.get('/', (req, res) => {
+	(async () => {
+		const connectResult = await connect(pgClient);
+		console.log('... connect successful:', connectResult);
 	})();
-	*/
 	res.send(`... PostgreSQL access from Node.js`);
 	console.log('... root access.');
 });
